7209a29da5b998547b70e37d44c699c162448233
James Carney <jmcarneyiii@gmail.com>
2021-04-26 19:18:12 -0400

    add router to main app module and register pages in router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import { HomePage, InPage, OutPage } from 'pages';
+import { StoreProvider } from 'store';
 
 
 const App = () => (
-  <div>Hello, world</div>
+  <StoreProvider>
+    <Router>
+      <h1>Hello, formatter</h1>
+      <div>
+        <Link to="/">Home</Link>
+        {' '}
+        <Link to="/in">In</Link>
+        {' '}
+        <Link to="/out">Out</Link>
+      </div>
+      <div style={{ width: '500px' }}>
+        <Switch>
+          <Route path="/in"><InPage /></Route>
+          <Route path="/out"><OutPage /></Route>
+          <Route path="/"><HomePage /></Route>
+        </Switch>
+      </div>
+    </Router>
+  </StoreProvider>
 );
 
 export default App;
